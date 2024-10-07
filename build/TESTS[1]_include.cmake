7f7848c971745f112a5014cb027cbbf1b3052b7a
Dmitriy <142345381+DmitriyShelihov@users.noreply.github.com>
2024-10-08 02:28:27 +0300

    Delete build directory

--- build/TESTS[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/dmitriy/Рабочий стол/Владос/2Q_cache/build/TESTS[1]_tests.cmake")
-  include("/home/dmitriy/Рабочий стол/Владос/2Q_cache/build/TESTS[1]_tests.cmake")
-else()
-  add_test(TESTS_NOT_BUILT TESTS_NOT_BUILT)
-endif()
