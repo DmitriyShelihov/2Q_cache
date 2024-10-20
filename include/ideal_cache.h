881d42dd6c02da32382aaac317a6e35acedfea96
Dmitriy_Shelihov <Shelihovdima@yandex.ru>
2024-10-21 02:31:38 +0300

    Refactored ideal cache. Using set

--- include/ideal_cache.h
+++ include/ideal_cache.h
@@ -3,98 +3,88 @@
 #include <iostream>
 #include <unordered_map>
 #include <list>
+#include <set>
 
 enum Ideal_cache_input_res {
 	tick = 1,
 	miss = 0
 };
 
-struct page_info {
-	std::list <int>::iterator iter;
+struct Page{
+	int page_number;
 	int next_hit;
 };
 
-struct Node {
-	int page;
-	int next_hit;
+struct cmp {
+	bool operator() (struct Page a1, struct Page a2) const {
+		return a1.next_hit < a2.next_hit;
+	}
 };
 
 class Ideal_cache {
 	private: 
-		std::list <int> id_list;
-		std::unordered_map <int, struct page_info> id_umap;
-		size_t id_list_size;
-
+		size_t cache_size;
+		std::set <struct Page, cmp> p_set;
+		
+		std::unordered_map <int, int> p_umap;
 	public: 
-		Ideal_cache (size_t list_sz) 
-			: id_list_size(list_sz) {} 
+		Ideal_cache (size_t cache_sz) 
+			: cache_size(cache_sz) {} 
 		Ideal_cache_input_res insert_page (int page_number, int next_hit) {
-			if (next_hit == -1 && id_umap.find(page_number) == id_umap.end()) {
+			struct Page cur_page = {page_number, next_hit};
+			if (next_hit == -1) {		
+				if (p_umap.find(page_number) != p_umap.end()) {
+					for (auto i = p_set.begin(), ie = p_set.end(); i != ie; ++i) {
+						if (i->page_number == cur_page.page_number) {
+							p_set.erase(i);
+							break;
+						}
+					}
+					p_umap.erase(page_number);
+					return tick;
+				} 
 				return miss;
 			}
-
-            if (next_hit > cur_max && id_list.size() >= id_list_size) {
-              	return miss;
-          	}
-
-			if (next_hit == -1) {					// => new_max
-				id_list.erase((id_umap[page_number]).iter);
-				id_umap.erase(page_number);
-					
-				int new_cur_max = -1;
-
-				for (auto i = id_umap.begin(), ie = id_umap.end(); i != ie; ++i) {
-					if ((i->second).next_hit > new_cur_max) {
-						new_cur_max = (i->second).next_hit;
-						latest_page = *((i->second).iter);
+			if (p_umap.find(page_number) != p_umap.end()) {
+				for (auto i = p_set.begin(), ie = p_set.end(); i!= ie; ++i) {
+					if (i->page_number == page_number) {
+						p_set.erase(i);
+						break;
 					}
 				}
-				cur_max = new_cur_max;
-				return tick;
-			}
-			
-			if (id_umap.find(page_number) != id_umap.end()) {
-				if (next_hit > cur_max) {
-					cur_max = next_hit;
-					latest_page = page_number;
-				}
-				(id_umap[page_number]).next_hit = next_hit;
+				p_set.insert(cur_page);
+				p_umap[page_number] = next_hit;
 				return tick;
 			} 
-			if (id_list.size() < id_list_size) {
-				if (next_hit > cur_max) {
-					cur_max = next_hit;
-					latest_page = page_number;
-				}
-				id_list.push_front(page_number);
-				id_umap[page_number] = {id_list.begin(), next_hit};
+			if (p_set.size() < cache_size) {
+				p_set.insert(cur_page);
+				p_umap[page_number] = next_hit;
 				
 				return miss;
 			} 
-			//=>new_max
-			id_list.erase((id_umap[latest_page]).iter);
-			id_umap.erase(latest_page);
+			if (next_hit > (*p_set.rbegin()).next_hit) 
+				return miss;
+			p_umap.erase((*p_set.rbegin()).page_number);
+			
+			Page pg_max = *p_set.rbegin();
 
-			id_list.push_front(page_number);
-			id_umap[page_number] = {id_list.begin(), next_hit};
+			for (auto i = p_set.begin(), ie = p_set.end(); i != ie; ++i) {
+				if (i->page_number == pg_max.page_number) {
+					p_set.erase(i);
+					break;
+				}
+			}
 
-			int new_cur_max = -1;
- 
-            for (auto i = id_umap.begin(), ie = id_umap.end(); i != ie; ++i) {
-              	if ((i->second).next_hit > new_cur_max) {
-                  	new_cur_max = (i->second).next_hit;
-                  	latest_page = *((i->second).iter);
-               	}
-          	}
-         	cur_max = new_cur_max;
+			p_set.insert(cur_page);
+			p_umap[page_number] = next_hit;
 
 			return miss;
 		}
 			
 		void coloured_dump() {
               std::cout << "\033[0;33m" << "IDEAL_CACHE:\n" << "\033[0;0m";
-              for (std::list <int>::iterator i = id_list.begin(); i != id_list.end(); ++i) 
-                  std::cout << "\033[0;34m" << "[" << "\033[0;0m" << *i << "\033[0;34m" << "]" << "\033[0;0m";
+              for (std::set <struct Page, cmp>::iterator i = p_set.begin(), ie = p_set.end(); i != ie; ++i) 
+                  std::cout << "\033[0;34m" << "[" << "\033[0;0m" << i->page_number << "(" << i->next_hit << ")" << "\033[0;34m" << "]" << "\033[0;0m";
               std::cout << std::endl;
         };
 };
