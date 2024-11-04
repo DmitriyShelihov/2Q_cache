75dc1265429e58a1806fa5c5c5551d23651c3ebe
Dmitriy_Shelihov <Shelihovdima@yandex.ru>
2024-11-04 20:48:30 +0300

    Added stable input to ideal_cahce

--- src/ideal_cache.cpp
+++ src/ideal_cache.cpp
@@ -1,5 +1,9 @@
 #include "ideal_cache.h"
 
+static bool is_number(std::string& str) {
+	return !str.empty() && (str.find_first_not_of("-0123456789") == str.npos);
+}
+
 std::unordered_map <int, std::list <int>> predict_pages(const int buf[], int npages) {
 	std::unordered_map <int, std::list <int>> Ideal_map;
  
@@ -15,15 +19,51 @@ std::unordered_map <int, std::list <int>> predict_pages(const int buf[], int npa
 }
 
 int main() {
-    size_t cache_sz = 0;
-    size_t npages = 0;
+    std::string cache_sz_buf;
+	std::cin >> cache_sz_buf;
+
+	if (!is_number(cache_sz_buf)) {
+		std::cout << "Cache size must be an integer!" << std::endl;
+		return 1;
+	}
+	int try_cache_sz = std::atoi(cache_sz_buf.c_str());
+
+	if (try_cache_sz <= 0) {
+		std::cout << "Cache size must be > 0!" << std::endl;
+		return 1;
+	}
+
+	size_t cache_sz = try_cache_sz;
 	
-	std::cin >> cache_sz >> npages;
-	
+	std::string npages_buf;
+	std::cin >> npages_buf;
+
+	if (!is_number(npages_buf)) {
+    	std::cout << "Number of requests must be an integer!" << std::endl;
+        return 1; 
+  	}   
+    int try_npages = std::atoi(npages_buf.c_str());
+      
+    if (try_npages < 0) {
+   		std::cout << "Number of requests must be >= 0!" << std::endl;
+        return 1;
+  	}
+      
+    size_t npages = try_npages;
+
 	int buf[npages];
 
-	for (int i = 0; i < npages; i++)
-		std::cin >> buf[i];
+	std::string page_buf;
+
+	for (int i = 0; i < npages; i++) {
+		std::cin >> page_buf;
+
+		if (!is_number(page_buf)) {
+			std::cout << "Request number must be an integer\n" << std::endl;
+			return 1;
+		}
+		buf[i] = std::atoi(page_buf.c_str());
+	}
 
 	std::unordered_map<int, std::list<int>> Ideal_map = predict_pages(buf, npages);
 	Ideal_cache cache (cache_sz);
