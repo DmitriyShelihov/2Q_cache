35006e9b8f7134d5b63d748228971c732ea03218
Dmitriy_Shelihov <Shelihovdima@yandex.ru>
2025-01-26 04:08:51 +0300

    Key -> const KeyT&, divide and conquer in insert

--- include/Cache.h
+++ include/Cache.h
@@ -16,43 +16,44 @@ class Cache {
 	std::unordered_map<KeyT, KeyTor> lumap_;
 	
 	std::unordered_map<KeyT, T> page_umap;
-	T (*slow_get_page_)(KeyT key);
+	T (*slow_get_page_)(const KeyT& key);
+
+	void lru_hit(const KeyT& key);
+	void fifo_hit(const KeyT& key);
+	void cache_miss(const KeyT& key);
 public:
-	explicit Cache(T (*slow_get_page)(KeyT key), size_t size = 0) 
+	explicit Cache(T (*slow_get_page)(const KeyT& key), size_t size = 0) 
 		: slow_get_page_(slow_get_page), size_(size) {}
-	void insert(KeyT key);
+	void insert(const KeyT& key);
+	
 	static int hits_;
 };
 
 template <typename T, typename KeyT>
-int Cache<T, KeyT>::hits_ = 0;
+void Cache<T, KeyT>::fifo_hit(const KeyT& key) {
+	this->fifo_.erase(this->fumap_[key]);
+
+	if (this->lru_.size() == this->size_) {
+		this->page_umap_.erase(this->lru_.back());
+		this->lumap_.erase(this->lru_.back());
+		this->lru_.pop_back();
+	}
+	this->lru_.push_front(key);
+	this->lumap_[key] = this->lru_.begin();
+	this->fumap_.erase(key);
+	++Cache<T, KeyT>::hits_;
+}
 
 template <typename T, typename KeyT>
-void Cache<T, KeyT>::insert(KeyT key) {
-	if (this->fumap_.find(key) != this->fumap_.end()) {
-		this->fifo_.erase(this->fumap_[key]);
+void Cache<T, KeyT>::lru_hit(const KeyT& key) {
+	this->lru_.erase(this->lumap_[key]);
+	this->lru_.push_front(key);
+	this->lumap_[key] = this->lru_.begin();
+	++Cache<T, KeyT>::hits_;	
+}
 
-		if (this->lru_.size() == this->size_) {
-			this->page_umap_.erase(this->lru_.back());
-			this->lumap_.erase(this->lru_.back());
-			this->lru_.pop_back();
-		}
-		this->lru_.push_front(key);
-		this->lumap_[key] = this->lru_.begin();
-		this->fumap_.erase(key);
-		++Cache<T, KeyT>::hits_;
-		return;
-	}
-	if (this->lumap_.find(key) != this->lumap_.end()) {
-		this->lru_.erase(this->lumap_[key]);
-		this->lru_.push_front(key);
-		this->lumap_[key] = this->lru_.begin();
-		++Cache<T, KeyT>::hits_;
-		return;
-	}
-
-	this->page_umap_[key] = slow_get_page(key);
-
+template <typename T, typename KeyT>
+void Cache<T, KeyT>::cache_miss(const KeyT& key) {
 	if (this->fifo_.size() < this->size_) {
 		this->fifo_.push_front(key);
 		this->fumap_[key] = this->fifo_.begin();
@@ -64,5 +65,20 @@ void Cache<T, KeyT>::insert(KeyT key) {
 	this->page_umap_.erase(last);
 	this->fifo_.push_front(key);
 	this->fumap_[key] = this->fifo_.begin();
+	return;
+	
+}
+
+template <typename T, typename KeyT>
+void Cache<T, KeyT>::insert(const KeyT& key) {
+	if (this->fumap_.find(key) != this->fumap_.end()) { 
+		this->fifo_hit(key); return; 
+	}
+	if (this->lumap_.find(key) != this->lumap_.end()) {
+		this->lru_hit(key); return;
+	}
+
+	this->page_umap_[key] = slow_get_page(key);
+	this->cache_miss(key);
 	return;
 }
