5e3eb640209a5291e95d73531fc23cf7172b655c
Dmitriy_Shelihov <Shelihovdima@yandex.ru>
2025-02-17 20:54:37 +0300

    inf cycle on 2 10 1 2 1 2 1 2 1 2 1 2

--- include/ideal_cache.h
+++ include/ideal_cache.h
@@ -1,5 +1,6 @@
 #pragma once 
 
+#include <iostream>
 #include <unordered_map>
 #include <set>
 #include <list>
@@ -10,7 +11,6 @@ template <typename KeyT, typename Compare = std::less<KeyT>>
 class IdealCache {
 	using PairT = std::pair<KeyT, size_t>;
 	using ListT = std::list<size_t>;
-
 private:
 	size_t size_;
 	struct cmp_for_set;
@@ -23,76 +23,77 @@ private:
 
 	struct cmp_for_set {
 		bool operator() (const PairT& a, const PairT& b) const{
-			return cmp_(a.second, b.second);
+			return a.second < b.second;
 		}
 	};
 
 private:
 	inline void map_keys(const std::vector<KeyT>& keys);
 	inline bool insert_page(const KeyT& key, size_t next_hit);
+    inline void set_delete_by_key(const KeyT& key);
+    inline void cache_add(const KeyT& key, size_t next_hit);
 public:
 	explicit IdealCache(const std::vector<KeyT>& keys, size_t size = 0, const Compare& cmp = Compare()) 
-		: size_(size), cmp_(cmp), keys_(keys), set_(cmp_for_set(cmp_)) { map_keys(keys); }
+		: size_(size), cmp_(cmp), keys_(keys) { map_keys(keys); std::cout << "ctor ended\n"; }
 	inline size_t get_ans();
 };
 
 template <typename KeyT, typename Compare>
 void IdealCache<KeyT, Compare>::map_keys(const std::vector<KeyT>& keys) {
+    printf("in mapping keys\n");
 	for (size_t i = 1, ie = keys.size()+1; i < ie; ++i) {
-		this->data_[keys[i-1]].push_front(i);
+        printf("here\n");
+		data_[keys[i-1]].push_front(i);
 	}
 	for (auto& i : this->data_) { i.second.push_front(0); }
 }
 
 template <typename KeyT, typename Compare>
-bool IdealCache<KeyT, Compare>::insert_page(const KeyT& key, size_t next_hit) {
-	auto& umap_ = this->umap_; 
-	auto& set_ = this->set_;
+void IdealCache<KeyT, Compare>::set_delete_by_key(const KeyT& key) {
+    auto i = set_.begin();
+    while (!cmp_(i->first, key)) { ++i; }
+    set_.erase(i); 
+}
 
-	if (umap_.find(key) != umap_.end()) {
-		for (auto i: set_) {
-			if (cmp_(i.first, key)) {
-				set_.erase(i);
-				break;
-			}
-		}
-		if (next_hit == 0) { 
-			umap_.erase(key);
-		} else {
-			set_.insert({key, next_hit});
-			umap_[key] = next_hit;
-		}
+template <typename KeyT, typename Compare>
+void IdealCache<KeyT, Compare>::cache_add(const KeyT& key, size_t next_hit) {
+    set_.insert({key, next_hit});
+    umap_[key] = next_hit;
+}
+
+template <typename KeyT, typename Compare>
+bool IdealCache<KeyT, Compare>::insert_page(const KeyT& key, size_t next_hit) { 
+    printf("inserting page\n");
+    auto it = umap_.find(key); 
+    if (next_hit == 0 && it != umap_.end()) {
+		set_delete_by_key(key);
+        umap_.erase(key);
 		return true;
-	}
-	if (next_hit == 0) {
+    }
+    if (next_hit == 0) { return false; }
+    
+    if (it != umap_.end()) {
+        set_delete_by_key(key);
+        cache_add(key, next_hit);
+        return true;
+    }
+    if (set_.size() < size_) {
+		cache_add(key, next_hit);
 		return false;
 	}
-	if (set_.size() < this->size_) {
-		set_.insert({key, next_hit});
-		umap_[key] = next_hit;
-		return false;
-	}
-	if (next_hit > set_.rbegin()->second)
-		return false;
-	
-	umap_.erase(set_.rbegin()->first);
-			
-	auto max_iter = set_.rbegin();
-
-	for (auto i: set_) {
-		if (cmp_(i.first, max_iter->first)) {
-			set_.erase(i);
-			break;
-		}
-	}
-	set_.insert({key, next_hit});
-	umap_[key] = next_hit;
-	return false;
+    auto r_it = set_.rbegin();
+	if (next_hit > r_it->second) { return false; }
+    umap_.erase(r_it->first);
+    set_delete_by_key(r_it->first);
+    cache_add(key, next_hit);
+    return false;
 }
 
 template <typename KeyT, typename Compare>
 size_t IdealCache<KeyT, Compare>::get_ans() {
-	for (auto i: keys_) {
+    printf("before cycle\n");
+    for (auto i: keys_) {
+        printf("here\n");
 		if (data_[i].size() > 0) {
 			data_[i].pop_back();
 		}
--- src/ideal_cache.cpp
+++ src/ideal_cache.cpp
@@ -1,86 +1,23 @@
 #include "../include/ideal_cache.h"
-
-static bool is_number(std::string& str) {
-	return !str.empty() && (str.find_first_not_of("-0123456789") == str.npos);
-}
-
-std::unordered_map <int, std::list <int>> predict_pages(const int buf[], int npages) {
-	std::unordered_map <int, std::list <int>> Ideal_map;
- 
-  	for (int i = 0; i < npages; i++) {
-		int page = buf[i];
-     
- 		(Ideal_map[page]).push_front(i);
-   	}
-   	for (auto i = Ideal_map.begin(), ie = Ideal_map.end(); i != ie; ++i) {
-		(i->second).push_front(-1);
-   	}
-  	return Ideal_map;
-}
+#include <iostream>
+#include <cassert>
 
 int main() {
-    std::string cache_sz_buf;
-	std::cin >> cache_sz_buf;
-
-	if (!is_number(cache_sz_buf)) {
-		std::cout << "Cache size must be an integer!" << std::endl;
-		return 1;
-	}
-	int try_cache_sz = std::atoi(cache_sz_buf.c_str());
-
-	if (try_cache_sz <= 0) {
-		std::cout << "Cache size must be > 0!" << std::endl;
-		return 1;
-	}
-
-	size_t cache_sz = try_cache_sz;
-	
-	std::string npages_buf;
-	std::cin >> npages_buf;
-
-	if (!is_number(npages_buf)) {
-    	std::cout << "Number of requests must be an integer!" << std::endl;
-        return 1; 
-  	}   
-    int try_npages = std::atoi(npages_buf.c_str());
-      
-    if (try_npages < 0) {
-   		std::cout << "Number of requests must be >= 0!" << std::endl;
-        return 1;
-  	}
-      
-    size_t npages = try_npages;
-
-	int buf[npages];
-
-	std::string page_buf;
-
-	for (int i = 0; i < npages; i++) {
-		std::cin >> page_buf;
-
-		if (!is_number(page_buf)) {
-			std::cout << "Request number must be an integer\n" << std::endl;
-			return 1;
-		}
-		buf[i] = std::atoi(page_buf.c_str());
-	}
-
-	std::unordered_map<int, std::list<int>> Ideal_map = predict_pages(buf, npages);
-	Ideal_cache cache (cache_sz);
-	
-	int page = 0 ;
-	int ticks = 0;
-
-	for (int i = 0; i < npages; i++) {
-		page = buf[i];   
- 		if ((Ideal_map[page]).size() > 0)
- 			(Ideal_map[page]).pop_back();
- 		if ((int)cache.insert_page(page, (Ideal_map[page]).back()) == 1)
- 			ticks++;
- 	}
-	
-    std::cout << ticks << std::endl;
-    return 0;
+    int npages;
+    size_t sz;
+    std::cin >> sz >> npages;
+    assert(std::cin.good());
+    std::vector<int> buf(npages);
+    
+    for (int i = 0; i < npages; ++i) { 
+        int page;
+        std::cin >> page;
+        assert(std::cin.good());
+        buf[i] = page;
+    }
+    IdealCache<int> id(buf, sz);
+    std::cout << "before getting ans\n";
+    std::cout << id.get_ans() << std::endl;
 }
 
 
